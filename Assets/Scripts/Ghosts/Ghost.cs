460d53cc5b5b3f0510dfce9442f4ec8da145af5d
vvrvvr <vover@mail.ru>
2023-02-14 16:37:41 +0400

    F! host logic and ghost animator

--- Assets/Scripts/Ghosts/Ghost.cs
+++ Assets/Scripts/Ghosts/Ghost.cs
@@ -1,24 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    public float timetoRestart = 1f;
+    public float timetoRestart = 10f;
     private float _currentTime = 0f;
+    private Animator _animator;
+    private static readonly int Disappear = Animator.StringToHash("disappear");
+    private bool _isDisappear = false;
 
-    void Start()
+    private void Awake()
     {
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _currentTime += Time.deltaTime;
-        if (_currentTime >= timetoRestart)
+        if (!_isDisappear)
         {
-            _currentTime = 0f;
-            gameObject.SetActive(false);
+            _currentTime += Time.deltaTime;
+            if (_currentTime >= timetoRestart)
+            {
+                _isDisappear = true;
+                _currentTime = 0f;
+                _animator.SetBool(Disappear, _isDisappear);
+            }
         }
+       
+    }
+
+    public void DeactivateGhost()
+    {
+        _isDisappear = false;
+        _animator.SetBool(Disappear, _isDisappear);
+        gameObject.SetActive(false);
     }
 }
